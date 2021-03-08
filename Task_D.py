693b35440b51dfb85f4d36d9586e46d3af1df715
Di-peep <52866436+Di-peep@users.noreply.github.com>
2021-03-09 01:13:10 +0200

    Update Task_D.py
    Тести пройшла.

--- Task_D.py
+++ Task_D.py
@@ -10,8 +10,11 @@ for i in range(t):
         if stek[j] in A and stek[j+1] in B and stek[j] != remember:
             remember = stek[j]
             stek = stek[:j] + stek[j+1] + stek[j] + stek[j+2:]
+            # print(i, j, ": ", stek[:j] + " " + stek[j] + " " + stek[j+1] + " " + stek[j+2:])
 
-    if stek == B + A or i > t:
+    if stek == B + A or i > t:  # виглядає як діч, але я вже не чіпав
+        # print("STOP")
         break
 
 print(stek)
+
